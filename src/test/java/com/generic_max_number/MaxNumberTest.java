a33bfd49621d774fbf9a3b5d0dcc87f05776a333
DattaDhebe <dattadhebe75@gmail.com>
2020-05-06 22:05:59 +0000

    Added. 'usecase2.1' find maximum number from floating numbers.

--- src/test/java/com/generic_max_number/MaxNumberTest.java
+++ src/test/java/com/generic_max_number/MaxNumberTest.java
@@ -7,20 +7,27 @@ import org.junit.Test;
 public class MaxNumberTest {
 
     @Test
-    public void maxAt_First_Position() {
+    public void integerMaxNumber_AtFirst_Position() {
         MaxNumber<Integer> max = new MaxNumber<>(10, 4, 7);
         Assert.assertEquals(10, max.findMaxNumber().intValue());
     }
 
     @Test
-    public void maxAt_Second_Position() {
+    public void integerMaxNumber_AtSecond_Position() {
         MaxNumber<Integer> max = new MaxNumber<>(4, 10, 7);
         Assert.assertEquals(10, max.findMaxNumber().intValue());
     }
 
     @Test
-    public void maxAt_Third_Position() {
+    public void intgerMaxNumber_AtThird_Position() {
         MaxNumber<Integer> max = new MaxNumber<>(4, 7, 10);
         Assert.assertEquals(10, max.findMaxNumber().intValue());
     }
+
+    @Test
+    public void floatMaxNumber_AtFirst_Position() {
+        MaxNumber<Float> max = new MaxNumber<>(1.3f, 1.2f, 1.1f);
+        Assert.assertEquals(1.3f, max.findMaxNumber(), 0.0f);
+    }
+
 }

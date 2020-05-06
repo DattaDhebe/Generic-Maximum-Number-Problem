0d9ae65a8bf284380c62659560d51408dd67c684
DattaDhebe <dattadhebe75@gmail.com>
2020-05-06 21:54:47 +0000

    Rafactor. Created Method to return Maximum Number.

--- src/main/java/com/generic_max_number/MaxNumber.java
+++ src/main/java/com/generic_max_number/MaxNumber.java
@@ -1,6 +1,23 @@
 package com.generic_max_number;
 
 public class MaxNumber<T extends  Comparable> {
+    T[] numbers;
 
+    public MaxNumber(T... numbers) {
+        this.numbers = numbers;
+    }
+
+    public T findMaxNumber(){
+        return findMaxNumber(this.numbers);
+    }
+
+    private static <T extends  Comparable> T findMaxNumber(T... values) {
+        T maxNumber = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if ( maxNumber.compareTo(values[i]) < 0 )
+                maxNumber = values[i];
+        }
+        return maxNumber;
+    }
 
 }
